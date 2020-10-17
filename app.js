c2f5c610cbf430ea6e136851a3dceedebf49d847
Franco <phanfranco@yahoo.com>
2020-10-16 20:30:20 -0700

    Added forloop.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var lowerCaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var upperCaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var numberChar= ['1','2','3','4','5','6','7','8','9','0']
@@ -9,22 +11,26 @@ var numbers = document.getElementById('numbers')
 var sChars = document.getElementById('sChars')
 var passwordDisplay = document.getElementById('passwordDisplay')
 
-
-console.log(sChars)
-
-pwCriteria.addEventListener('submit', e =>{
-    e.preventDefault()
+btn.addEventListener('click', function(e){
     var characterAmount = numOfChars.value
-    var uppCase = upperCase.Checked
-    var nums = numbers.Checked
-    var spChars = sChars.Checked
+    var uppCase = upperCase.checked
+    var nums = numbers.checked
+    var spChars = sChars.checked
     var password = generatePassword(characterAmount, uppCase, nums, spChars )
     passwordDisplay.innerText = password
+    e.preventDefault()
 })
-console.log(characterAmount)
 
-function generatePassword(characterAmount, upperCase, numbers, sChars ){
-    let password= lowerCaseChar
+function generatePassword(characterAmount, uppCase, nums, spChars ){
+    let charCodes = lowerCaseChar
+    if (uppCase) charCodes = charCodes.concat(upperCaseChar)
+    if (spChars) charCodes = charCodes.concat(sCharChar)
+    if (nums) charCodes = charCodes.concat(numberChar)
+
+    var defaultPass = ''
+        for (let i = 0; i < characterAmount; i++) {
+            var randomChar = Math.floor(Math.random() * charCodes.length); 
+            defaultPass = defaultPass + charCodes[randomChar]
+       }
+    return defaultPass
 }
-
-console.log(lowerCaseChar)
